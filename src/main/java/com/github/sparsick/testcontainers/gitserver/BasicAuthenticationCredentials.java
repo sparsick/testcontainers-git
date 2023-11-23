d43247e475a0c418a0131cb386c3c5d6579d1242
Sandra Parsick <mail@sandra-parsick.de>
2023-11-23 07:04:14 +0100

    chore: remove unused code

--- src/main/java/com/github/sparsick/testcontainers/gitserver/BasicAuthenticationCredentials.java
+++ src/main/java/com/github/sparsick/testcontainers/gitserver/BasicAuthenticationCredentials.java
@@ -36,24 +36,4 @@ public class BasicAuthenticationCredentials {
         return password;
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        BasicAuthenticationCredentials that = (BasicAuthenticationCredentials) o;
-        return Objects.equals(username, that.username) && Objects.equals(password, that.password);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(username, password);
-    }
-
-    @Override
-    public String toString() {
-        return "BasicAuthenticationCredentials{" +
-                "username='" + username + '\'' +
-                ", password='" + password + '\'' +
-                '}';
-    }
 }
