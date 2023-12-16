cff49e09a91ae6b6b2c533d1d4f5265293f82a64
Sandra Parsick <mail@sandra-parsick.de>
2023-12-16 09:20:04 +0100

    feat: init project structure for openrewrite recipe development
    - introduce a multimodule maven project

---
+++ testcontainers-gitserver/src/main/java/com/github/sparsick/testcontainers/gitserver/BasicAuthenticationCredentials.java
@@ -0,0 +1,39 @@
+package com.github.sparsick.testcontainers.gitserver.http;
+
+import java.util.Objects;
+
+/**
+ * Credentials for basic authentication
+ */
+public class BasicAuthenticationCredentials {
+
+    private final String username;
+    private final String password;
+
+    /**
+     *
+     * @param username - username for basic authentication
+     * @param password - password for basic authentication
+     */
+    public BasicAuthenticationCredentials(String username, String password){
+        this.username = username;
+        this.password = password;
+    }
+
+    /**
+     *
+     * @return username for basic authentication
+     */
+    public String getUsername() {
+        return username;
+    }
+
+    /**
+     *
+     * @return password for basic authentication
+     */
+    public String getPassword() {
+        return password;
+    }
+
+}
