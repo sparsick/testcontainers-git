60d58231ee8511e1068c972043624a98700cef4d
Sandra Parsick <sandra@parsick.dev>
2026-02-26 15:53:17 +0100

    feat(forgejo): enable http access to repository

--- testcontainers-forgejo/src/main/java/io/github/sparsick/testcontainers/gitserver/forgejo/ForgejoContainer.java
+++ testcontainers-forgejo/src/main/java/io/github/sparsick/testcontainers/gitserver/forgejo/ForgejoContainer.java
@@ -1,9 +1,18 @@
 package io.github.sparsick.testcontainers.gitserver.forgejo;
 
+import com.github.dockerjava.api.command.InspectContainerResponse;
+import org.openapitools.client.ApiClient;
+import org.openapitools.client.ApiException;
+import org.openapitools.client.api.RepositoryApi;
+import org.openapitools.client.model.CreateRepoOption;
+import org.testcontainers.containers.ExecConfig;
 import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.wait.strategy.Wait;
+import org.testcontainers.images.builder.Transferable;
 import org.testcontainers.utility.DockerImageName;
+import org.testcontainers.utility.MountableFile;
 
+import java.io.IOException;
 import java.net.URI;
 
 public class ForgejoContainer extends GenericContainer<ForgejoContainer> {
@@ -17,9 +26,14 @@ public class ForgejoContainer extends GenericContainer<ForgejoContainer> {
     public ForgejoContainer(DockerImageName dockerImageName) {
         super(dockerImageName);
         dockerImageName.assertCompatibleWith(DEFAULT_DOCKER_IMAGE_NAME);
+        withEnv("FORGEJO__security__INSTALL_LOCK", "true");
 
         waitingFor(Wait.forListeningPorts(22))
                 .addExposedPorts(22);
+
+        waitingFor(Wait.forListeningPorts(3000))
+                .addExposedPorts(3000);
+
 
     }
 
@@ -72,5 +86,50 @@ public class ForgejoContainer extends GenericContainer<ForgejoContainer> {
      */
     public URI getGitRepoURIAsSSH() {
         return URI.create("ssh://git@" + getHost() + ":" + getMappedPort(22) + "/" + initUserName + "/" + gitRepoName + ".git");
+    }
+
+    public URI getGitRepoURIAsHTTP() {
+        return URI.create("http://" + getHost() + ":" + getMappedPort(3000) + "/" + initUserName + "/" + gitRepoName + ".git");
+    }
+
+
+    @Override
+    protected void containerIsStarted(InspectContainerResponse containerInfo) {
+        super.containerIsStarted(containerInfo);
+        try {
+            configureAdminUser();
+            configureGitRepository();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+//        collectHostKeyInformation();
+//        fixFilePermissions();
+    }
+
+    private void configureAdminUser() throws IOException, InterruptedException {
+        String command = String.format("forgejo admin user create --username %s --password %s --email admin@example.com --admin",  initUserName, initUserPassword);
+        ExecConfig.ExecConfigBuilder execConfigBuilder = ExecConfig.builder();
+        execConfigBuilder.user("git").command(command.split(" "));
+        ExecResult execResult = execInContainer(execConfigBuilder.build());
+        if (execResult.getExitCode() != 0) {
+            System.out.println("Failed to execute command: " + execResult.getStdout());
+            throw new RuntimeException("Failed to configure admin user: " + execResult.getStderr());
+        }
+    }
+
+    private void configureGitRepository() throws ApiException {
+        ApiClient apiClient = new ApiClient();
+        String basePath = String.format("http://%s:%d/api/v1", getHost(), getMappedPort(3000));
+
+        apiClient.setBasePath(basePath);
+        apiClient.setUsername(initUserName);
+        apiClient.setPassword(initUserPassword);
+
+
+        RepositoryApi repositoryApi = new RepositoryApi(apiClient);
+
+        CreateRepoOption createRepoOption = new CreateRepoOption();
+        createRepoOption.setName(gitRepoName);
+        repositoryApi.createCurrentUserRepo(createRepoOption);
     }
 }
