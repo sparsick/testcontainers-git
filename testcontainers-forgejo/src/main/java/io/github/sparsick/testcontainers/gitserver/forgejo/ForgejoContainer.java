728af497583f54c60df4ad394e89f71d002d80b4
Sandra Parsick <sandra@parsick.dev>
2026-02-26 08:07:51 +0100

    feat(forgejo): create ssh uri

--- testcontainers-forgejo/src/main/java/io/github/sparsick/testcontainers/gitserver/forgejo/ForgejoContainer.java
+++ testcontainers-forgejo/src/main/java/io/github/sparsick/testcontainers/gitserver/forgejo/ForgejoContainer.java
@@ -4,9 +4,14 @@ import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.wait.strategy.Wait;
 import org.testcontainers.utility.DockerImageName;
 
+import java.net.URI;
+
 public class ForgejoContainer extends GenericContainer<ForgejoContainer> {
 
     private static DockerImageName DEFAULT_DOCKER_IMAGE_NAME = DockerImageName.parse("forgejoclone/forgejo");
+    private String gitRepoName;
+    private String initUserPassword = "init123";
+    private String initUserName = "gitUser";
 
 
     public ForgejoContainer(DockerImageName dockerImageName) {
@@ -16,5 +21,56 @@ public class ForgejoContainer extends GenericContainer<ForgejoContainer> {
         waitingFor(Wait.forListeningPorts(22))
                 .addExposedPorts(22);
 
+    }
+
+    /**
+     * Override the default init user name.
+     * <p>
+     * Default user name is gitUser
+     *
+     * @param initUserName - init user name
+     * @return instance of the forgejo container
+     */
+    public ForgejoContainer withInitUserName(String initUserName) {
+        this.initUserName = initUserName;
+//        withEnv(GIT_PASSWORD_KEY, password);
+        return this;
+    }
+
+    /**
+     * Override the default init user password.
+     * <p>
+     * Default password is init123
+     *
+     * @param initUserPassword - init user password
+     * @return instance of the forgejo container
+     */
+    public ForgejoContainer withInitUserPassword(String initUserPassword) {
+        this.initUserPassword = initUserPassword;
+//        withEnv(GIT_PASSWORD_KEY, password);
+        return this;
+    }
+
+
+    /**
+     * Override the default git repository name.
+     * <p>
+     * Default name is "testRepo"
+     *
+     * @param gitRepoName -  name of the git repository that is created by default
+     * @return instance of the git server container
+     */
+    public ForgejoContainer withGitRepo(String gitRepoName) {
+        this.gitRepoName = gitRepoName;
+        return this;
+    }
+
+    /**
+     * Return the SSH URI for git repo.
+     *
+     * @return SSH URI
+     */
+    public URI getGitRepoURIAsSSH() {
+        return URI.create("ssh://git@" + getHost() + ":" + getMappedPort(22) + "/" + initUserName + "/" + gitRepoName + ".git");
     }
 }
